c33baa53fe71a36d65f4d85bbaac251e38400dbd
jonathanmeneses <jonathan.m.meneses@gmail.com>
2023-10-19 10:44:10 -0500

    Shading functionality

--- script.js
+++ script.js
@@ -1,66 +1,61 @@
-let container = document.getElementById('grid-element')
+let container = document.getElementById('grid-element');
 let initialDimShape = 16;
 const squareDimensions = 700;
 
-// Create Grid, starting with rows
-
+// Function to create a grid
 function createGrid(n) {
     const gridContainer = document.createElement('div');
-    gridContainer.setAttribute('class', 'grid-container')
-    gridContainer.style.width = `${squareDimensions}px`
-    gridContainer.style.height = `${squareDimensions}px`
-    container.appendChild(gridContainer)
-    for (y = 0; y < n; y++) {
-        const row = document.createElement('div')
-        row.setAttribute('class', 'row')
-        gridContainer.appendChild(row)
-        for (x = 0; x < n; x++) {
-            const square = document.createElement('div')
-            square.setAttribute('class', 'pixel')
-            square.style.backgroundColor = 'white'
-            row.appendChild(square)
+    gridContainer.setAttribute('class', 'grid-container');
+    gridContainer.style.width = `${squareDimensions}px`;
+    gridContainer.style.height = `${squareDimensions}px`;
+    container.appendChild(gridContainer);
 
-            row.addEventListener('mouseover', (e) => {
-                if (e.target.style.backgroundColor == 'white') {
-                    e.target.style.backgroundColor = random_rgba()
+    for (let y = 0; y < n; y++) {
+        const row = document.createElement('div');
+        row.setAttribute('class', 'row');
+        gridContainer.appendChild(row);
+
+        for (let x = 0; x < n; x++) {
+            const square = document.createElement('div');
+            square.setAttribute('class', 'pixel');
+            square.style.backgroundColor = 'white';
+            square.style.width = `${(squareDimensions / n)}px`;
+            square.style.height = `${(squareDimensions / n)}px`;
+
+            square.addEventListener('mouseover', (e) => {
+                if (e.target.style.backgroundColor === 'white') {
+                    e.target.style.backgroundColor = random_rgba();
                 }
+            });
 
-
-
-            })
-
-            square.style.width = `${(squareDimensions / n)}px`
-            square.style.height = `${(squareDimensions / n)}px`
-
-
+            row.appendChild(square);
         }
     }
 }
 
+// Function to generate a random color
+function random_rgba() {
+    let o = Math.round, r = Math.random, s = 255;
+    return `rgba(${o(r() * s)}, ${o(r() * s)}, ${o(r() * s)}, ${r().toFixed(1)})`;
+}
+
+// Function to create a new grid
+function newGrid() {
+    let grid = document.querySelector('.grid-container');
+    let gridDimensions = prompt('What is the size (size x size) of the grid you want? (max of 64)');
+    gridDimensions = Number(gridDimensions);
+
+    gridToCreate = Math.max(Math.min(gridDimensions, 64), 1); // Ensure grid size is between 1 and 64
+    grid.remove();
+    createGrid(gridToCreate);
+}
 
 // Add Button to initiate prompt
-const dimButton = document.createElement('button')
-dimButton.setAttribute('class', 'config-button')
-dimButton.innerText = 'Configure Grid'
-dimButton.addEventListener('click', newGrid)
-container.insertBefore(dimButton, container.firstChild)
+const dimButton = document.createElement('button');
+dimButton.setAttribute('class', 'config-button');
+dimButton.innerText = 'Configure Grid';
+dimButton.addEventListener('click', newGrid);
+container.insertBefore(dimButton, container.firstChild);
 
-
-createGrid(initialDimShape)
-
-function newGrid() {
-    let grid = document.querySelector('.grid-container')
-    let gridDimensions = prompt('What is the size (size x size) of the grid you want? (max of 64)')
-    gridDimensions = Number(gridDimensions)
-
-    gridToCreate = Math.max(Math.min(gridDimensions, 64), 1)
-    console.log(gridToCreate)
-    grid.remove()
-    createGrid(gridToCreate)
-
-}
-
-function random_rgba() {
-    var o = Math.round, r = Math.random, s = 255;
-    return 'rgba(' + o(r() * s) + ',' + o(r() * s) + ',' + o(r() * s) + ',' + r().toFixed(1) + ')';
-}
+// Initial grid creation
+createGrid(initialDimShape);
