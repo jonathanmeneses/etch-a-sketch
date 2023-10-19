6fe9f63e592801e3bc9f9c93ef59bedaad1f37c8
jonathanmeneses <jonathan.m.meneses@gmail.com>
2023-10-18 21:04:22 -0500

    Add styling updates

--- script.js
+++ script.js
@@ -1,13 +1,16 @@
 let container = document.getElementById('grid-element')
-
-for (y = 0; y < 16; y++) {
+let dimensionShapes = 64;
+const squareDimensions = 400;
+for (y = 0; y < dimensionShapes; y++) {
     const row = document.createElement('div')
     row.setAttribute('class', 'row')
     container.appendChild(row)
-    for (x = 0; x < 16; x++) {
+    for (x = 0; x < dimensionShapes; x++) {
         const square = document.createElement('div')
         square.setAttribute('class', 'pixel')
         row.appendChild(square)
+        square.style.width = `${(squareDimensions / dimensionShapes)}px`
+        square.style.height = `${(squareDimensions / dimensionShapes)}px`
 
         row.addEventListener('mouseover', (e) => {
             e.target.style.backgroundColor = 'black'
